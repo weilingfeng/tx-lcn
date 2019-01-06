98a03e3e8975b154e354aa27478625bca9ee0952
yizhishang <285206405@qq.com>
2019-01-06 17:31:15 +0800

    origin/springcloud-2.0.4 --修改ModelNameServiceImpl getIp方法

--- transaction-springcloud/src/main/java/com/codingapi/tx/springcloud/service/impl/ModelNameServiceImpl.java
+++ transaction-springcloud/src/main/java/com/codingapi/tx/springcloud/service/impl/ModelNameServiceImpl.java
@@ -1,15 +1,20 @@
 package com.codingapi.tx.springcloud.service.impl;
 
 import com.codingapi.tx.listener.service.ModelNameService;
+import com.codingapi.tx.netty.utils.IpAddressUtils;
 import com.codingapi.tx.springcloud.listener.ServerListener;
 import com.lorne.core.framework.utils.encode.MD5Util;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 /**
  * Created by lorne on 2017/7/12.
@@ -24,6 +29,7 @@ public class ModelNameServiceImpl implements ModelNameService {
     @Autowired
     private ServerListener serverListener;
 
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     private String host = null;
 
@@ -32,14 +38,39 @@ public class ModelNameServiceImpl implements ModelNameService {
         return modelName;
     }
 
+    /**
+     * 获取ip地址，先获取hostname，如果获取的是正常的ip且不为127.0.0.1则返回，否则遍历网卡，获取有效ip
+     * @return
+     */
     private String getIp() {
-        if (host == null) {
+        if (StringUtils.isBlank(host)) {
             try {
                 host = InetAddress.getLocalHost().getHostAddress();
-            } catch (UnknownHostException e) {
+                if (IpAddressUtils.isIpAddress(host) && !"127.0.0.1".equals(host)) {
+                    logger.info("getIp1={}", host);
+                    return host;
+                } else {
+                    // 读网卡的ip地址
+                    Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+                    for (; n.hasMoreElements(); ) {
+                        NetworkInterface e = n.nextElement();
+                        logger.info("Interface:{}", e.getName());
+                        Enumeration<InetAddress> a = e.getInetAddresses();
+                        for (; a.hasMoreElements(); ) {
+                            InetAddress addr = a.nextElement();
+                            if (IpAddressUtils.isIpAddress(addr.getHostAddress()) && !"127.0.0.1".equals(addr.getHostAddress())) {
+                                logger.info("addr.getHostAddress={}", addr.getHostAddress());
+                                return addr.getHostAddress();
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                logger.error("getIp.e={}", e);
                 e.printStackTrace();
             }
         }
+        logger.info("getIp2={}", host);
         return host;
     }
 
@@ -55,7 +86,7 @@ public class ModelNameServiceImpl implements ModelNameService {
             port = serverListener.getServerPort();
             count++;
 
-            if(count==2000){
+            if (count == 2000) {
                 throw new RuntimeException("get server port error.");
             }
         }
