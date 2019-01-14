d0d205b2e61442b178500da81446f875b85ca832
yizhishang <285206405@qq.com>
2019-01-14 14:19:18 +0800

    自定义5.0.0版本 --启动与 spring-retry 冲突

--- tx-spi-sleuth-springcloud/src/main/java/com/codingapi/tx/spi/sleuth/springcloud/ribbon/customizer/RibbonFirstRestTemplateCustomizer.java
+++ tx-spi-sleuth-springcloud/src/main/java/com/codingapi/tx/spi/sleuth/springcloud/ribbon/customizer/RibbonFirstRestTemplateCustomizer.java
@@ -3,6 +3,7 @@ package com.codingapi.tx.spi.sleuth.springcloud.ribbon.customizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
+import org.springframework.cloud.client.loadbalancer.RetryLoadBalancerInterceptor;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -19,20 +20,22 @@ import java.util.List;
  */
 @Component
 public class RibbonFirstRestTemplateCustomizer implements RestTemplateCustomizer {
-
-
+    @Autowired(required = false)
     private LoadBalancerInterceptor loadBalancerInterceptor;
 
-    @Autowired
-    public RibbonFirstRestTemplateCustomizer(LoadBalancerInterceptor loadBalancerInterceptor) {
-        this.loadBalancerInterceptor = loadBalancerInterceptor;
-    }
+    @Autowired(required = false)
+    private RetryLoadBalancerInterceptor retryLoadBalancerInterceptor;
+
 
     @Override
     public void customize(RestTemplate restTemplate) {
         List<ClientHttpRequestInterceptor> list = new ArrayList<>(restTemplate.getInterceptors());
-        list.add(0,loadBalancerInterceptor);
+        if (loadBalancerInterceptor != null) {
+            list.add(0, loadBalancerInterceptor);
+        }
+        if (retryLoadBalancerInterceptor != null) {
+            list.add(0, retryLoadBalancerInterceptor);
+        }
         restTemplate.setInterceptors(list);
     }
-
 }
