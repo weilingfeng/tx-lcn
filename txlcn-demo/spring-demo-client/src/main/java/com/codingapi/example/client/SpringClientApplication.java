e7a02cb06faba0f6b73f5cb1eda9a58796747695
yizhishang <285206405@qq.com>
2019-01-17 16:57:47 +0800

    自定义5.0.0.RC1版本 -- 添加demo

--- txlcn-demo/spring-demo-client/src/main/java/com/codingapi/example/client/SpringClientApplication.java
+++ txlcn-demo/spring-demo-client/src/main/java/com/codingapi/example/client/SpringClientApplication.java
@@ -2,7 +2,7 @@ package com.codingapi.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Description:
@@ -11,7 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author ujued
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class SpringClientApplication {
 
     public static void main(String[] args) {
